77a6440be99eb71acc015ecfac39718bc5aa75d5
Dan Mon <Dan Mon>
2017-05-04 12:32:09 -0500

    admin bar hide

--- functions.php
+++ functions.php
@@ -180,3 +180,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+// default hide admin bar
+
+add_filter('show_admin_bar','__return_false');
