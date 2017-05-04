234c73f02d0b0babf8e93cc4155617f346c90f12
Dan Mon <Dan Mon>
2017-05-04 10:21:15 -0500

    header debuging

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	
 <header> <!-- Choice container fluid or default container -->
 	
-	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+	<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
    </button>
